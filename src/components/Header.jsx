629fa7a827a3b295090eecac8d8a9392f82b852d
sumonDev9 <sumonmit678@gmail.com>
2025-03-13 00:05:14 +0530

    Add Wendy One font

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,13 @@ import React from 'react';
 
 const Header = () => {
     return (
-        <div>
-            
+        <div className='relative w-11/12 mx-auto bg-[#EEE6C1]'>
+            {/* <div>
+                <h2></h2>
+            </div>
+            <div></div>
+            <div></div> */}
+            <h1>This is Header page</h1>
         </div>
     );
 };
