f97b96fb2872cd5b04fa42a3883627df4d2f1c53
sumonDev9 <sumonmit678@gmail.com>
2025-03-12 23:45:50 +0530

    Set up React project with React Router and Tailwind CSS

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React, { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import router from './router/router.jsx'
+import { RouterProvider } from 'react-router-dom'
+
+createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+  <RouterProvider router={router} />
+</React.StrictMode>
+)
