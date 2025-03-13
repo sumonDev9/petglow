28ef6e1ad8edeef1290400451f2e7b01a1104abd
sumonDev9 <sumonmit678@gmail.com>
2025-03-13 16:40:36 +0530

    Add shampoo section with images, cards, and Buy Now button

--- src/components/WhyChooseUs.jsx
+++ src/components/WhyChooseUs.jsx
@@ -8,9 +8,15 @@ import eafrer from '../assets/choose/eafrer.png'
 import Group from '../assets/choose/Group.png'
 import VectorRight from '../assets/choose/VectorRight.png'
 import Untitled from '../assets/choose/Untitled.png'
+import dog from '../assets/choose/dog.png'
+import Fluffnshine from '../assets/choose/Fluffnshine.png'
+import bg from '../assets/choose/bg.png'
+import edsf from '../assets/choose/edsf.png'
+import aroow from '../assets/choose/aroow.png'
 const WhyChooseUs = () => {
     return (
-        <div className='relative flex flex-col md:flex-row justify-between'>
+   <section className='pb-10'>
+         <div className='relative flex flex-col md:flex-row justify-between'>
         {/* choose */}
         <div className='relative'>
            
@@ -38,6 +44,33 @@ const WhyChooseUs = () => {
             </div>
         </div>
     </div>
+
+    {/* smapoo  */}
+    <div className='relative grid grid-cols-1 md:grid-cols-3'>
+        <div className='mx-auto z-10'>
+            <img src={dog} className='w-full h-full ml-64 mt-14' alt="" />
+        </div>
+        <div className='relative'>
+            <img src={bg} alt="" />
+            <img src={Fluffnshine} className='h-[500px] z-10 absolute top-0 right-24' alt="" />
+            <img src={edsf} className='absolute bottom-0 right-0 z-20' alt="" />
+            <img src={vecteezy} className='absolute bottom-0 -left-54 z-20' alt="" />
+            <img src={aroow} className='absolute top-0 -right-36' alt="" />
+            <img src={aroow} className='absolute -bottom-24 -right-36' alt="" />
+        </div>
+        <div className='mr-32 space-y-10 mt-[170px]'>
+            <div className='bg-white shadow p-6 border-2 border-[#B69659] rounded-xl'>
+                <h1 className='text-2xl md:text-3xl text-[#000000]'>Choose a Natural <br />& Safe Formula</h1>
+            </div>
+            <div className='bg-white mt-20 shadow  p-6 border-2 border-[#B69659] rounded-xl'>
+                <h1 className='text-2xl md:text-2xl text-[#000000]'>Prioritize Vet-Approved<br />& Breed-Safe Shampoos</h1>
+            </div>
+        </div>
+    </div>
+    <div className='flex justify-center items-center'>
+                <button className='bg-[#AD5915DB] bg-opacity-86 text-white text-2xl cursor-pointer lg:text-3xl px-6 py-2 rounded-full'>Buy Now</button>
+            </div>
+   </section>
     
     );
 };
