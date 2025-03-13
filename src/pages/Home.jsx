03cd5da47037585120b82f7d1ce9971319604358
sumonDev9 <sumonmit678@gmail.com>
2025-03-13 09:17:41 +0530

    Add Benefits component with images, grid layout, and styling

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from '../components/Header';
 import Features from '../components/Features';
+import Benefits from '../components/Benefits';
 
 const Home = () => {
     return (
         <div>
            <Header></Header>
            <Features></Features>
+           <Benefits></Benefits>
         </div>
     );
 };
